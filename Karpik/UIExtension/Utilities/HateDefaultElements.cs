1a835f4f4bfdf3219658f4dbc04c414441bdde8f
Karpik <artem19051664@gmail.com>
2024-08-24 04:46:27 +0300

    Add ForceUpdate() for ScrollView

---
+++ Karpik/UIExtension/Utilities/HateDefaultElements.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Karpik.UIExtension
+{
+    public static class HateDefaultElements
+    {
+        public static void ForceUpdate(this ScrollView scroll)
+        {
+            scroll.style.height = scroll.resolvedStyle.height;
+            scroll.schedule.Execute(() =>
+            {
+                var fakeOldRect = Rect.zero;
+                var fakeNewRect = scroll.layout;
+
+                using var evt = GeometryChangedEvent.GetPooled(fakeOldRect, fakeNewRect);
+                evt.target = scroll.contentContainer;
+                scroll.contentContainer.SendEvent(evt);
+            });
+        }
+    }
+}
