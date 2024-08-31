63cbfc433e35a9a09b6984d7c660de1376d5f1d3
Karpik <artem19051664@gmail.com>
2024-08-31 16:10:08 +0300

    Improve ScrollView's ForceUpdate

--- Karpik/UIExtension/Utilities/HateDefaultElements.cs
+++ Karpik/UIExtension/Utilities/HateDefaultElements.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Karpik.UIExtension
@@ -7,16 +8,37 @@ namespace Karpik.UIExtension
     {
         public static void ForceUpdate(this ScrollView scroll)
         {
-            scroll.style.height = scroll.resolvedStyle.height;
+            if (scroll.childCount > 0)
+            {
+                var child = scroll.Children().First();
+                child.schedule.Execute(() =>
+                {
+                    var fakeOldRect = new Rect(child.layout.position + Vector2.one, child.layout.size + Vector2.one);
+                    var fakeNewRect = child.layout;
+
+                    using var evt = GeometryChangedEvent.GetPooled(fakeOldRect, fakeNewRect);
+                    evt.target = child.contentContainer;
+                    child.contentContainer.SendEvent(evt);
+                });
+            }
+
+            var v = scroll.verticalScroller.value;
+            var h = scroll.horizontalScroller.value;
             scroll.schedule.Execute(() =>
             {
-                var fakeOldRect = Rect.zero;
+                var fakeOldRect = new Rect(scroll.layout.position + Vector2.one, scroll.layout.size + Vector2.one);
                 var fakeNewRect = scroll.layout;
 
                 using var evt = GeometryChangedEvent.GetPooled(fakeOldRect, fakeNewRect);
                 evt.target = scroll.contentContainer;
                 scroll.contentContainer.SendEvent(evt);
             });
+            
+            scroll.schedule.Execute(() =>
+            {
+                scroll.verticalScroller.value = v > scroll.verticalScroller.highValue ? scroll.verticalScroller.highValue : v;
+                scroll.horizontalScroller.value = h > scroll.horizontalScroller.highValue ? scroll.horizontalScroller.highValue : h;
+            }).StartingIn(1);
         }
     }
 }
