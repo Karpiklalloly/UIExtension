7fa5e033719b34de4b3f5b015ce561cb6d206a34
Karpik <artem19051664@gmail.com>
2024-09-01 23:29:20 +0300

    Add tooltip support

---
+++ Karpik/UIExtension/Elements/TooltipElement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Karpik.UIExtension
+{
+    public class TooltipElement : BetterVisualElement
+    {
+        public string Title
+        {
+            get => _title.text;
+            set => _title.text = value;
+        }
+
+        public string Description
+        {
+            get => _description.text;
+            set => _description.text = value;
+        }
+
+        private Label _title;
+        private Label _description;
+
+        public TooltipElement()
+        {
+            _title = new Label();
+            _title.name = "Title";
+            _description = new Label();
+            _description.name = "Title";
+            
+            hierarchy.Add(_title);
+            hierarchy.Add(_description);
+            
+            style.BorderRadius(5f);
+            style.Border(2f, Color.yellow);
+            style.backgroundColor = Color.black;
+            style.opacity = new StyleFloat(0.75f);
+            style.position = new StyleEnum<Position>(Position.Absolute);
+            style.justifyContent = new StyleEnum<Justify>(Justify.FlexStart);
+            style.alignContent = new StyleEnum<Align>(Align.Stretch);
+            //style.minHeight = new StyleLength(20);
+            style.minWidth = new StyleLength(60);
+            style.maxWidth = new StyleLength(300);
+            style.color = Color.white;
+        }
+
+        public void Show()
+        {
+            style.visibility = new StyleEnum<Visibility>(Visibility.Visible);
+        }
+
+        public void Hide()
+        {
+            style.visibility = new StyleEnum<Visibility>(Visibility.Hidden);
+        }
+    }
+}
---
+++ Karpik/UIExtension/Manipulators/TooltipManipulator.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Karpik.UIExtension
+{
+    public class TooltipManipulator : PointerManipulator
+    {
+        private static TooltipElement _tooltip = new();
+
+        public Mode FollowMode
+        {
+            get => _followMode;
+            set
+            {
+                _followMode = value;
+            }
+        }
+        
+        private VisualElement _container;
+        private Mode _followMode = Mode.FollowCursor;
+
+        private Func<string> _getTitle;
+        private Func<string> _getDescription;
+        private Func<VisualElement> _tooltipContainer;
+
+        public TooltipManipulator(
+            Func<string> getTitle,
+            Func<string> getDescription,
+            Func<VisualElement> tooltipContainer,
+            Mode mode = Mode.FollowCursor)
+        {
+            _getTitle = getTitle;
+            _getDescription = getDescription;
+            _tooltipContainer = tooltipContainer;
+            _followMode = mode;
+        }
+        
+        protected override void RegisterCallbacksOnTarget()
+        {
+            target.RegisterCallback<PointerMoveEvent>(OnPointerMove);
+            target.RegisterCallback<PointerLeaveEvent>(OnPointerLeave);
+            target.RegisterCallback<PointerEnterEvent>(OnPointerEnter);
+        }
+
+        protected override void UnregisterCallbacksFromTarget()
+        {
+            target.UnregisterCallback<PointerMoveEvent>(OnPointerMove);
+            target.UnregisterCallback<PointerLeaveEvent>(OnPointerLeave);
+            target.UnregisterCallback<PointerEnterEvent>(OnPointerEnter);
+        }
+
+        private void OnPointerMove(PointerMoveEvent e)
+        {
+            if (_container != null)
+            {
+                SetPosition(e);
+            }
+        }
+
+        private void OnPointerEnter(PointerEnterEvent e)
+        {
+            _container = _tooltipContainer?.Invoke();
+            _tooltip.Title = _getTitle?.Invoke();
+            _tooltip.Description = _getDescription?.Invoke();
+            target.CapturePointer(e.pointerId);
+            _tooltip.Show();
+            if (_container.hierarchy.Children().Contains(_tooltip)) return;
+            
+            _container.hierarchy.Add(_tooltip);
+        }
+
+        private void OnPointerLeave(PointerLeaveEvent e)
+        {
+            if (target.HasPointerCapture(e.pointerId))
+            {
+                target.ReleasePointer(e.pointerId);
+            }
+            
+            _tooltip.Hide();
+            
+            if (!_container.Children().Contains(_tooltip)) return;
+            
+            _container.hierarchy.Remove(_tooltip);
+        }
+
+        private void SetPosition(PointerMoveEvent e)
+        {
+            switch (_followMode)
+            {
+                case Mode.FollowCursor:
+                    _tooltip.transform.position =
+                        _container.WorldToLocal(e.position) + Vector2.one + new Vector2(10, 10);
+                    break;
+                case Mode.Centralized:
+                    _tooltip.transform.position = _container.WorldToLocal(target.LocalToWorld(Vector2.zero));
+                    _tooltip.transform.position += new Vector3(
+                        target.style.width.value.value / 2,
+                        target.style.height.value.value / 2);
+                    break;
+            }
+        }
+        
+        public enum Mode
+        {
+            FollowCursor,
+            Centralized
+        }
+    }
+}
